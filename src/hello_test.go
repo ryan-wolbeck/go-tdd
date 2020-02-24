3a76e15ec561a8b6a7e7c5326058c938c621fd1f
Ryan <ryan.wolbeck@chrobinson.com>
2020-02-24 14:39:17 -0700

    add french

--- src/hello_test.go
+++ src/hello_test.go
@@ -7,21 +7,33 @@ func TestHello(t *testing.T) {
 	assertCorrectMessage := func(t *testing.T, got, want string) {
 		t.Helper()
 		if got != want {
-			t.Errorf("got %q ant %q", got, want)
+			t.Errorf("got %q want %q", got, want)
 		}
 	}
 
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Ryan")
+		got := Hello("Ryan", "")
 		want := "Hello, Ryan"
 
 		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, World"
 
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Juan", "Spanish")
+		want := "Hola, Juan"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("David", "French")
+		want := "Bonjour, David"
 		assertCorrectMessage(t, got, want)
 	})
 
