4bac78dacfc1273bdc3c524c9dc71ff70251becc
Ryan <ryan.wolbeck@chrobinson.com>
2020-04-06 11:39:55 -0500

    sum across multiple slices

--- src/arrays_slices/sum.go
+++ src/arrays_slices/sum.go
@@ -1,9 +1,22 @@
 package arrays
 
+//Sum : Sums all the  numbers in an array
 func Sum(nums []int) int{
 	sum := 0
 	for _, number := range nums {
 		sum += number
 	}
 	return sum
+}
+
+//SumAll : Takes in multiple arrays and returns the sums
+func SumAll(numbersToSum ...[]int) []int{
+	lengthOfNumbers := len(numbersToSum)
+	sums := make([]int, lengthOfNumbers)
+	
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
+	}
+
+	return sums
 }
